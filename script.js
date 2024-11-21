38d4c5340e6513507ddf8a1c7ad1c589287c05b7
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-21 12:31:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function shortcut(s1, s2) {
   // your code here
+  if (!s1 || !s2) {
+    return '';
+  }
+  // Return the first character of each string concatenated
+  return s1[0] + s2[0];
 }
-
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
